191e395af9f96784eb85219a87d3ddbd9730dede
chenweigao <297859260@qq.com>
2018-09-15 20:05:39 +0800

    update map

--- app/static/js/trail.js
+++ app/static/js/trail.js
@@ -5,68 +5,77 @@ var myChart = echarts.init(document.getElementById('main'), 'vintage');
 //var data_modify = data_original.split(" ")
 
 // console.log(data_original);
-var mapData=`{ "type": "FeatureCollection", "features": [ { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [ [ [ 0, -0 ], [ 9, -0 ], [ 9, -20 ], [ 0, -20 ], [ 0, -0 ], [ 0, -0 ] ] ] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 9, -9 ], [ 7.4, -9 ], [ 7.4, -9.35 ], [ 9, -9.35 ], [ 9, -9 ] ]] } },  { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 0, -8.2 ], [ 1.3, -8.2 ], [ 1.3, -10.7 ], [ 2.7, -10.7 ], [ 2.7, -9 ], [ 6.2, -9 ], [ 6.2, -9.35 ], [ 2.8, -9.35 ], [ 2.8, -12.35 ], [ 0, -12.35 ], [ 0, -8.2 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 0, -2.85 ], [ -0.1, -2.85 ], [ -0.1, -4.83 ], [ 0, -4.83 ], [ 0, -2.85 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ -0.1, -4.83 ], [ -0.1, -7.84 ], [ 0, -7.84 ], [ -0.1, -4.83 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 1.5, 0 ], [ 1.5, -0.1 ], [ 4.5, -0.1 ], [ 4.5, 0 ], [ 1.5, 0 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 9, -2.9 ], [ 9.1, -2.9 ], [ 9.1, -5.47 ], [ 9, -5.47 ], [ 9, -2.9 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 7, -2.5 ], [ 7, -2.8 ], [ 7, -2.65 ], [ 6.7, -2.65 ], [ 6.7, -2.5 ], [ 6.7, -2.8 ], [ 7, -2.5 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 7, -8 ], [ 7, -8.3 ], [ 7, -8.15 ], [ 6.7, -8.15 ], [ 6.7, -8 ], [ 6.7, -8.3 ], [ 7, -8 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 0.3, -2.5 ], [ 0.3, -2.8 ], [ 0.3, -2.65 ], [ 0, -2.65 ], [ 0, -2.5 ], [ 0, -2.8 ], [ 0.3, -2.5 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 0.3, -8 ], [ 0.3, -8.3 ], [ 0.3, -8.15 ], [ 0, -8.15 ], [ 0, -8 ], [ 0, -8.3 ], [ 0.3, -8 ] ]] } } ] } `
+var mapData = `{ "type": "FeatureCollection", "features": [ { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [ [ [ 0, -0 ], [ 9, -0 ], [ 9, -20 ], [ 0, -20 ], [ 0, -0 ], [ 0, -0 ] ] ] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 9, -9 ], [ 7.4, -9 ], [ 7.4, -9.35 ], [ 9, -9.35 ], [ 9, -9 ] ]] } },  { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 0, -8.2 ], [ 1.3, -8.2 ], [ 1.3, -10.7 ], [ 2.7, -10.7 ], [ 2.7, -9 ], [ 6.2, -9 ], [ 6.2, -9.35 ], [ 2.8, -9.35 ], [ 2.8, -12.35 ], [ 0, -12.35 ], [ 0, -8.2 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 0, -2.85 ], [ -0.1, -2.85 ], [ -0.1, -4.83 ], [ 0, -4.83 ], [ 0, -2.85 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ -0.1, -4.83 ], [ -0.1, -7.84 ], [ 0, -7.84 ], [ -0.1, -4.83 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 1.5, 0 ], [ 1.5, -0.1 ], [ 4.5, -0.1 ], [ 4.5, 0 ], [ 1.5, 0 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 9, -2.9 ], [ 9.1, -2.9 ], [ 9.1, -5.47 ], [ 9, -5.47 ], [ 9, -2.9 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 7, -2.5 ], [ 7, -2.8 ], [ 7, -2.65 ], [ 6.7, -2.65 ], [ 6.7, -2.5 ], [ 6.7, -2.8 ], [ 7, -2.5 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 7, -8 ], [ 7, -8.3 ], [ 7, -8.15 ], [ 6.7, -8.15 ], [ 6.7, -8 ], [ 6.7, -8.3 ], [ 7, -8 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 0.3, -2.5 ], [ 0.3, -2.8 ], [ 0.3, -2.65 ], [ 0, -2.65 ], [ 0, -2.5 ], [ 0, -2.8 ], [ 0.3, -2.5 ] ]] } }, { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": [[ [ 0.3, -8 ], [ 0.3, -8.3 ], [ 0.3, -8.15 ], [ 0, -8.15 ], [ 0, -8 ], [ 0, -8.3 ], [ 0.3, -8 ] ]] } } ] } `
 // var jsonData = JSON.parse(data_original.replace(/ObjectId\(/g, '').replace(/\)/g, '').replace(/'/g, '"'))
 
 echarts.registerMap('indoor', mapData);
-
-var updateChart = function(){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/path.json', true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200 || xhr.status == 304) {
-            // 从服务器获得数据 
-            //fn.call(this, xhr.responseText);
-            console.log(this.responseText);
-            var pathData = JSON.parse(xhr.responseText.replace(/'/g, '"'));
-            
-            var pathList=[]
-            for(let i =0;i<pathData.length;i++){
-                let path=pathData[i];
-                var coords=[]
-                for(let j=0;j>path.length;j++){
-                    coords.push([path[j].x, path[j].y]);                    
-                }
-                pathList.push({coords:coords});
-            }
-            
-            myChart.setOption({
-              //backgroundColor: '#111',
-              geo: [{
-                roam: true,
-                itemStyle: {
-                  normal: {
-                    color: 'transparent',
-                    //borderColor: 'rgba(255,255,255,255)',
-                    borderWidth: 1
-                  }
-                },
-                map: 'indoor',
-                silent: true
-              }],
+console.log("ECHARTS");
+var updateChart = function () {
+  console.log("DATA");
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/path.json', true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200 || xhr.status == 304) {
 
 
-              series: [{
-                type: 'lines',
-                coordinateSystem: 'geo',
-                //data: lines,
-                data: pathList,
-                polyline: true,
-                lineStyle: {
-                  normal: {
-                    color: 'purple',
-                    opacity: 1,
-                    width: 1
-                  }
-                }
-              }]
-            });
-            
+      // 从服务器获得数据 
+      //fn.call(this, xhr.responseText);
+      console.log(this.responseText);
+      var pathData = JSON.parse(xhr.responseText.replace(/'/g, '"'));
+
+      var pathList = []
+      for (let i = 0; i < pathData.length; i++) {
+        let path = pathData[i];
+        var coords = []
+        for (let j = 0; j < path.length; j++) {
+          coords.push([path[j].x, path[j].y]);
         }
+        pathList.push({ coords: coords });
+      }
 
-        xhr.send();
+      console.log(pathList);
+
+      myChart.setOption({
+        backgroundColor: '#fff',
+        geo: [{
+          roam: true,
+          itemStyle: {
+            normal: {
+              color: 'transparent',
+              //borderColor: 'rgba(255,255,255,255)',
+              borderWidth: 1
+            }
+          },
+          map: 'indoor',
+          silent: true
+        }],
+
+
+        series: [{
+          type: 'lines',
+          coordinateSystem: 'geo',
+          //data: lines,
+          data: pathList,
+          polyline: true,
+          lineStyle: {
+            normal: {
+              color: 'purple',
+              opacity: 1,
+              width: 1
+            }
+          }
+        }]
+      });
+
     }
+
+  }
+  xhr.send();
 }
+
+//window.addEventListener('onload', function (event) {
+updateChart();
+//})
 
 // var xhr = new XMLHttpRequest();
 // xhr.open('GET', '/data.json', true);
